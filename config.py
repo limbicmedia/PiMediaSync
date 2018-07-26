e4db1b4b47d45e02f7e9b4f70ffaf83745509d20
sabjorn <info@sabjorn.net>
2018-07-25 18:56:27 -0700

    modifies config.py so that all lights in display have a minimum brightness when operating

--- config.py
+++ config.py
@@ -14,57 +14,57 @@ class Config():
     CHANNELS = [25, 26, 27, 22, 23, 24, 19, 20, 21, 16, 17, 18] # order of DMX channels
     LIGHTING_SEQUENCE = [
         {
-            'dmx_levels': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'dmx_levels': [64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 68
         },
         {
-            'dmx_levels': [255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'dmx_levels': [255, 64, 64, 64, 64, 64, 64, 64, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 92
         },
         {
-            'dmx_levels': [0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'dmx_levels': [64, 255, 64, 64, 64, 64, 64, 64, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 120
         },
         {
-            'dmx_levels': [0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'dmx_levels': [64, 64, 255, 64, 64, 64, 64, 64, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 140
         },
         {
-            'dmx_levels': [0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0],
+            'dmx_levels': [64, 64, 64, 255, 64, 64, 64, 64, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 162
         },
         {
-            'dmx_levels': [0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0],
+            'dmx_levels': [64, 64, 64, 64, 255, 64, 64, 64, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 192
         },
         {
-            'dmx_levels': [0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0],
+            'dmx_levels': [64, 64, 64, 64, 64, 255, 64, 64, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 256
         },
         {
-            'dmx_levels': [0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0],
+            'dmx_levels': [64, 64, 64, 64, 64, 64, 255, 64, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 290
         },
         {
-            'dmx_levels': [0, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0],
+            'dmx_levels': [64, 64, 64, 64, 64, 64, 64, 255, 64, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 318
         },
         {
-            'dmx_levels': [0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0],
+            'dmx_levels': [64, 64, 64, 64, 64, 64, 64, 64, 255, 64, 0, 0],
             'dmx_transition': 1,
             'end_time': 330
         },
         {
-            'dmx_levels': [0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0],
+            'dmx_levels': [64, 64, 64, 64, 64, 64, 64, 64, 64, 255, 0, 0],
             'dmx_transition': 1,
             'end_time': 360
         },
